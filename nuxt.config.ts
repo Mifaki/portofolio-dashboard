9c82d21fe4f6f2024d0d287daa5d53ab52552ad3
Mifaki <agustianto.d19@gmail.com>
2025-02-17 14:46:34 +0700

    feat(other): init repository config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+	compatibilityDate: '2024-11-01',
+	devtools: { enabled: true },
+	css: ['~/assets/css/main.css'],
+	app: {
+		head: {
+			charset: 'utf-8',
+			viewport: 'width=device-width, initial-scale=1',
+			title: 'Portofolio Dashboard',
+			meta: [
+				{
+					name: 'description',
+					content: "Management System for Ahmad Faiz's Portofollio Website",
+				},
+			],
+		},
+	},
+	modules: ['@nuxt/eslint'],
+	vite: { plugins: [tailwindcss()] },
+});
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+	<main class="flex h-screen w-full items-center justify-center">
+		<h1 class="text-3xl font-bold underline">Hello world!</h1>
+	</main>
+</template>
