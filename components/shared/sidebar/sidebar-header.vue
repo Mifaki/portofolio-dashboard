d1abb1fa9e0782dc27819632b5115c1fd3f7cdd7
Mifaki <agustianto.d19@gmail.com>
2025-12-25 21:45:09 +0700

    chore: start a clean base

--- components/shared/sidebar/sidebar-header.vue
+++
@@ -1,7 +0,0 @@
-<template>
-	<div class="flex items-center gap-4 px-4 py-6">
-		<img src="/img/logo.png" alt="logo" />
-		<h1 class="text-xxl font-semibold">Dashboard</h1>
-	</div>
-	<USeparator />
-</template>
